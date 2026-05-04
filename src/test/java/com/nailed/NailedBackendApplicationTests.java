b61749fd0941c28bc1b612793467e68f7453e114
offonoffdance-wq <offonoffdance@gmail.com>
2026-05-04 11:54:32 +0900

    create nailed backend project

---
+++ src/test/java/com/nailed/NailedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nailed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NailedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
