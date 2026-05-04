baac963191d5bde214122e8f1534a1f80a0e2c48
offonoffdance-wq <offonoffdance@gmail.com>
2026-05-04 11:54:32 +0900

    create nailed backend project

---
+++ src/main/java/com/nailed/NailedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nailed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NailedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NailedBackendApplication.class, args);
+	}
+
+}
